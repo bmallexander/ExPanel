c49d907e392888d76f150d8289987419a5f7a2f8
bmallexander <allexander.bergmans@outlook.com>
2024-08-20 16:40:36 +0200

    Added Router I,port

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const router = express.Router();
 const session = require('express-session');
 const passport = require('passport');
 const mongoose = require('mongoose');

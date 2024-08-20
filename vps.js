fb556cca75e6f61eed2a02a5ff26f453b231a185
bmallexander <allexander.bergmans@outlook.com>
2024-08-20 16:38:42 +0200

    VPS.JS Update

--- vps.js
+++ vps.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const Docker = require('dockerode');
 const User = require('./models/User');
 const VPS = require('./models/VPS');
+
+const docker = new Docker();
 
 router.post('/vps/create', async (req, res) => {
     const { name, os } = req.body;
@@ -13,12 +16,26 @@ router.post('/vps/create', async (req, res) => {
     }
 
     try {
+        // Create a new Docker container
+        const container = await docker.createContainer({
+            Image: image,
+            Cmd: ['/bin/sh'],  // Command to run inside the container
+            name: name,
+            Tty: true
+        });
+
+        // Start the container
+        await container.start();
+
+        // Save VPS details in the database
         const vps = new VPS({
             name,
             image,
-            userId: req.user._id
+            userId: req.user._id,
+            containerId: container.id // Store the Docker container ID
         });
         await vps.save();
+
         res.redirect('/dashboard');
     } catch (error) {
         console.error('Error creating VPS:', error);

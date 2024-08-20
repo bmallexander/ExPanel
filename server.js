a63d0a4f855eeb86184df3554dc0db0b7ada7600
bmallexander <allexander.bergmans@outlook.com>
2024-08-20 17:33:42 +0200

    Commented Out Router

--- server.js
+++ server.js
@@ -219,7 +219,7 @@ app.get('/', (req, res) => {
   res.render('index', { user: req.user });
 });
 
-app.use('/');
+//app.use('/');
 
 server.listen(3000, () => {
   console.log('Server running on http://localhost:3000');
